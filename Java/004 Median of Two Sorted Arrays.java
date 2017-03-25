2bff32bcf30e20001a4ec317d3078d06ef45a8d0
mengfeng113 <mengfeng113@gmail.com>
2017-03-25 13:39:22 -0700

    004 New solution

--- Java/004 Median of Two Sorted Arrays.java
+++ Java/004 Median of Two Sorted Arrays.java
@@ -26,11 +26,32 @@ public class Solution {
         if(sA + k/2 - 1 < A.length) mA = A[sA + k/2 - 1];
         if(sB + k/2 - 1 < B.length) mB = B[sB + k/2 - 1];
         
-        if(mA < mB){
+        if(mA < mB){//A.length + B.length >= k, so mA and mB cannot both be Integer.MAX_VALUE
             return getKth(A, sA + k/2, B, sB, k - k/2);
         }
         else{
             return getKth(A, sA, B, sB + k/2, k - k/2);
         }
     }
+}
+
+public class Solution {
+    public double findMedianSortedArrays(int A[], int B[]) {
+        int totalEle = A.length + B.length;
+		if (totalEle == 0) return 0;
+		if (totalEle % 2 == 1) return findKth(A, 0, A.length, B, 0, B.length, (totalEle + 1) / 2);
+		else return (findKth(A, 0, A.length, B, 0, B.length, totalEle/2) + 
+				findKth(A, 0, A.length, B, 0, B.length, totalEle/2 + 1))/2;
+    }
+    
+    private static double findKth(int A[], int sA, int m, int B[], int sB, int n, int k){
+		if (sA > A.length - 1)	return B[sB + k - 1];
+		if (sB > B.length - 1)	return A[sA + k - 1];
+		if (k == 1)	return Math.min(A[sA], B[sB]); // boundary
+		int numA = Math.min(k/2, m), numB = Math.min(k/2, n);
+		if (A[sA + numA - 1] <= B[sB + numB - 1])
+			return findKth(A, sA + numA, m - numA, B, sB, n, k - numA);
+		else
+			return findKth(A, sA, m, B, sB + numB, n - numB, k - numB);
+	}
 }
